fa4c8663fe11e548dc82db8228391a090727d71a
natalyatg <83861154+natalyatg@users.noreply.github.com>
2024-01-19 20:21:58 +0200

    Update exceptions.h

--- exceptions.h
+++ exceptions.h
@@ -8,7 +8,5 @@ class Exception: public std::exception{};
 class DataAlreadyExists: public Exception{};
 class DataNotExists: public Exception{};
 
-//class NodeDoesntExist: public Exception{};
-//class TreeHasData: public Exception{};
 
 #endif //EXCEPTIONS_H
